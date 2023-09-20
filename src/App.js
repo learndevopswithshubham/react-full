ef866968382f46b278836d17882c89e653f9aa0a
shubham <119154519+learndevopswithshubham@users.noreply.github.com>
2023-09-20 11:28:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
  
 function App() {
     return (
-        <h1> WELCOME TO OUR DEV BRANCH........................................... </h1>
+        <h1> WELCOME TO OUR DEV BRANCH........................................... New commit from dev branch </h1>
     );
 }
  
